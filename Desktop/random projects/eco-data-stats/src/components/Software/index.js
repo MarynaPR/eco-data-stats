a57dfc7cf7a5de8f2d9e28b9a63a111c89f33682
MarynaPR <pryadkamaryna@gmail.com>
2021-11-10 10:58:19 -0600

    code and colors updates

--- Desktop/random projects/eco-data-stats/src/components/Software/index.js
+++ Desktop/random projects/eco-data-stats/src/components/Software/index.js
@@ -6,9 +6,14 @@ function Software() {
     return (
         <section>
             <h3> See EcoDataStats Live</h3>
+            <br />
             <p>Interact with live dashboards in our gallery</p>
-            <p><a href="https://app.powerbi.com/links/SfvIwN5wjL?ctid=9cce8368-101c-4cb9-b1c6-20c964379df1&pbi_source=linkShare">View live Dashbord Gallery >></a></p>
-            <img src={reportImg} className="my-2" style={{ width: "100%" }} alt="cover" />
+            {/* <p><a href="https://app.powerbi.com/links/SfvIwN5wjL?ctid=9cce8368-101c-4cb9-b1c6-20c964379df1&pbi_source=linkShare">View live Dashbord Gallery >> </a></p> */}
+            <div className="powerBi">
+                <iframe title="portal" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiYzFlZTg5NTgtOWVmMS00NDZjLTkwY2UtMjEyM2U5YjZhNWViIiwidCI6IjljY2U4MzY4LTEwMWMtNGNiOS1iMWM2LTIwYzk2NDM3OWRmMSIsImMiOjN9" frameborder="0" allowFullScreen="true"></iframe>
+            </div>
+
+            {/* <img src={reportImg} className="my-2" style={{ width: "100%" }} alt="cover" /> */}
         </section>
     );
 };
