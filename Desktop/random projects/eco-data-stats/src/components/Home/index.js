affc923986728ba2685d924f9befbe3f30656624
SMKrizan <smkrizan@gmail.com>
2022-01-10 16:52:12 -0600

    added initial framework for industries on homepage

--- Desktop/random projects/eco-data-stats/src/components/Home/index.js
+++ Desktop/random projects/eco-data-stats/src/components/Home/index.js
@@ -1,64 +1,74 @@
 import React from 'react';
+import "./index.css";
 
 function Home() {
     return (
-       <div>
-        <h1>Select your industries of interest:</h1>
-            <section className="industries">
+       <section id="industries">
+        <h1>Select your industry of interest:</h1>
+            <div className="industry">
                 <div>
                     <p>Agribusiness</p>
                 </div>
-                <nav></nav>
-            </section>
-            <section className="industries">
+                <nav>
+                    <ul>
+                        <li>Agriculture</li>
+                        <li>Beverage</li>
+                        <li>Food</li>
+                        <li>Seafood</li>
+                        <li>Tobacco</li>
+                        <li>Wood</li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="industry">
                 <div>
                     <p>Construction & Real Estate</p>
                 </div>
                 <nav></nav>
-            </section>
-            <section className="industries">
+            </div>
+            <div className="industry">
                 <div>
                     <p>Consumer Goods & Retail</p>
                 </div>
                 <nav></nav>
-            </section>
-            <section className="industries">
+            </div>
+            <div className="industry">
                 <div>
                     <p>Heavy Industry</p>
                 </div>
                 <nav></nav>
-            </section>
-            <section className="industries">
+            </div>
+            <div className="industry">
                 <div>
                     <p>Manufacturing</p>
                 </div>
                 <nav></nav>
-            </section>
-            <section className="industries">
+            </div>
+            <div className="industry">
                 <div>
                     <p>ICT & Media</p>
                 </div>
                 <nav></nav>
-            </section>
-            <section className="industries">
+            </div>
+            <div className="industry">
                 <div>
                     <p>Pharmaceutical & Healthcare</p>
                 </div>
                 <nav></nav>
-            </section>
-            <section className="industries">
+            </div>
+            <div className="industry">
                 <div>
                     <p>Services</p>
                 </div>
                 <nav></nav>
-            </section>
-            <section className="industries">
+            </div>
+            <div className="industry">
                 <div>
                     <p>Financial Services</p>
                 </div>
                 <nav></nav>
-            </section>
-       </div>
+            </div>
+       </section>
     );
 }
 
